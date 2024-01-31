ee700f2e251266c9c84c16ea02eebdb8433bdc0c
DLMSweet <DLMSweet@totaldataloss.com>
2024-01-31 20:34:40 +0000

    Fixes #5, hopefully. When a key did not exist, an exception would be thrown that would cause the results to not show up.

--- lib/Manga.py
+++ lib/Manga.py
@@ -25,9 +25,25 @@ class Manga():
         self.data = self.api.make_request('manga/{}'.format(self.manga_id))
         if self.data is None:
             raise MangaNotFound
-        self.title = self.data["data"]["attributes"]["title"]["en"]
+        # Try to load the english title first, and failing that try the first available one.
+        possible_langs = self.data["data"]["attributes"]["title"].keys()
+        try:
+            self.title = self.data["data"]["attributes"]["title"]["en"]
+        except KeyError:
+             self.title = self.data["data"]["attributes"]["title"][possible_langs[0]]
         parser = bbcode.Parser(escape_html=False)
         parser.add_simple_formatter('spoiler', '<span class="spoiler">%(value)s</span>')
-        self.description = parser.format(self.data["data"]["attributes"]["description"]["en"])
+        # Check if a description exists first
+        possible_langs = None
+        if self.data["data"]["attributes"]["description"]:
+            possible_langs = self.data["data"]["attributes"]["description"].keys()
+            # Try to load the english one, because I'm english.
+            try:
+                self.description = parser.format(self.data["data"]["attributes"]["description"]["en"])
+            except KeyError:
+                # Failing that, load the first possible one.
+                self.description = parser.format(self.data["data"]["attributes"]["description"][possible_langs[0]])
+        else:
+            self.description = 'No Description'
         self.alt_titles = self.data["data"]["attributes"]["altTitles"]
 
